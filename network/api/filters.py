1ba693077f13ea89aff0d9a528c3d97ef8983845
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-06-30 02:58:47 +0200

    Fix API observation filter

--- network/api/filters.py
+++ network/api/filters.py
@@ -6,9 +6,7 @@ from network.base.models import Observation
 class ObservationViewFilter(django_filters.FilterSet):
     start = django_filters.IsoDateTimeFilter(name='start', lookup_expr='gte')
     end = django_filters.IsoDateTimeFilter(name='end', lookup_expr='lte')
-    norad = django_filters.NumberFilter(name='satellite__norad_cat_id',
-                                        lookup_expr='iexact')
 
     class Meta:
         model = Observation
-        fields = ['ground_station']
+        fields = ['ground_station', 'satellite__norad_cat_id']
