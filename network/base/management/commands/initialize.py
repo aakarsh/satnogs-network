8395eeea1d09727dd4339395fb32246288fd9df1
Corey Shields <cshields@gmail.com>
2018-08-12 11:07:40 -0400

    add more data in dev
    increase the amount of factory data created for dev purposes, so filters and pagination can be better tested.

--- network/base/management/commands/initialize.py
+++ network/base/management/commands/initialize.py
@@ -20,9 +20,9 @@ class Command(BaseCommand):
 
         # Create random fixtures for remaining models
         self.stdout.write("Creating fixtures...")
-        StationFactory.create_batch(10,
+        StationFactory.create_batch(40,
                                     antennas=(Antenna.objects.all().values_list('id', flat=True)))
-        ObservationFactory.create_batch(20)
+        ObservationFactory.create_batch(200)
         for _ in range(40):
             DemodDataFactory.create(payload_demod__data=generate_payload(),
                                     payload_demod__filename=generate_payload_name())
