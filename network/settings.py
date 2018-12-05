21bca4d7747a28dc8c07988fc6fc8212bc5bfd3b
Pierros Papadeas <pierros@papadeas.gr>
2018-12-05 22:51:26 +0200

    Fix defaults for scheduling time presets

--- network/settings.py
+++ network/settings.py
@@ -325,10 +325,10 @@ MAPBOX_TOKEN = config('MAPBOX_TOKEN', default='')
 
 # Observations settings
 # Datetimes in minutes for scheduling OPTIONS
-OBSERVATION_DATE_MIN_START = config('OBSERVATION_DATE_MIN_START', default=5, cast=int)
-OBSERVATION_DATE_MIN_END = config('OBSERVATION_DATE_MIN_END', default=65, cast=int)
+OBSERVATION_DATE_MIN_START = config('OBSERVATION_DATE_MIN_START', default=10, cast=int)
+OBSERVATION_DATE_MIN_END = config('OBSERVATION_DATE_MIN_END', default=370, cast=int)
 # Deletion range in minutes
-OBSERVATION_DATE_MAX_RANGE = config('OBSERVATION_DATE_MAX_RANGE', default=2880, cast=int)
+OBSERVATION_DATE_MAX_RANGE = config('OBSERVATION_DATE_MAX_RANGE', default=2890, cast=int)
 # Clean up threshold in days
 OBSERVATION_OLD_RANGE = config('OBSERVATION_OLD_RANGE', default=30, cast=int)
 
