558eb432fe814fe786790430b881d849086705f7
Pierros Papadeas <pierros@papadeas.gr>
2018-12-16 00:33:21 +0200

    Blur station status filters on stations page

--- network/static/js/stations.js
+++ network/static/js/stations.js
@@ -11,22 +11,22 @@ $(document).ready(function() {
 
     $('#stations-online').click(function() {
         $('.station-row:has(\'.label-online\')').toggle();
-        $(this).toggleClass('active');
+        $(this).toggleClass('active').blur();
     });
 
     $('#stations-testing').click(function() {
         $('.station-row:has(\'.label-testing\')').toggle();
-        $(this).toggleClass('active');
+        $(this).toggleClass('active').blur();
     });
 
     $('#stations-offline').click(function() {
         $('.station-row:has(\'.label-offline\')').toggle();
-        $(this).toggleClass('active');
+        $(this).toggleClass('active').blur();
     });
 
     $('#stations-future').click(function() {
         $('.station-row:has(\'.label-future\')').toggle();
-        $(this).toggleClass('active');
+        $(this).toggleClass('active').blur();
     });
 
 });
