a55107bda222e24a12393242392a91410ac9e2ac
Pierros Papadeas <pierros@papadeas.gr>
2019-01-07 19:05:05 +0200

    Filter observations on the API using their ID

--- network/api/filters.py
+++ network/api/filters.py
@@ -10,8 +10,8 @@ class ObservationViewFilter(FilterSet):
 
     class Meta:
         model = Observation
-        fields = ['ground_station', 'satellite__norad_cat_id', 'transmitter', 'vetted_status',
-                  'vetted_user']
+        fields = ['id', 'ground_station', 'satellite__norad_cat_id', 'transmitter',
+                  'vetted_status', 'vetted_user']
 
 
 class StationViewFilter(FilterSet):
