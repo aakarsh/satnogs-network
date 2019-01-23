beafdc50cf5468bc81f602da8062abb59ad351b5
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2019-01-23 20:44:41 +0200

    Make email settings configurable via environment
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- network/settings.py
+++ network/settings.py
@@ -63,9 +63,12 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25
-EMAIL_TIMEOUT = 300
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', default=300, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@satnogs.org')
 ADMINS = [
     ('SatNOGS Admins', DEFAULT_FROM_EMAIL)
