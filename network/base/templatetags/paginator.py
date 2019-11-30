211a7c525240ca07bf1090380f931846b36d4f63
Fabian P. Schmidt <kerel@mailbox.org>
2019-11-30 08:13:21 +0100

    templatetags/paginator: Fixes name collision (W0621)
    Fixes a redefined-outer-name (W0621) pylint error.
    
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- network/base/templatetags/paginator.py
+++ network/base/templatetags/paginator.py
@@ -12,8 +12,8 @@ def paginator(context, request, adjacent_pages=2):
     Adds pagination context variables for use in displaying first, adjacent and
     last page links.
     """
-    paginator = context['paginator']
     page_obj = context['page_obj']
+    num_pages = context['paginator'].num_pages
 
     # Widen the adjacency if near the start or near the end of the page set
     start_page = max(page_obj.number - adjacent_pages, 1)
@@ -21,15 +21,15 @@ def paginator(context, request, adjacent_pages=2):
         start_page = 1
 
     end_page = page_obj.number + adjacent_pages + 1
-    if end_page >= paginator.num_pages - 1:
-        end_page = paginator.num_pages + 1
+    if end_page >= num_pages - 1:
+        end_page = num_pages + 1
 
     # Generate a list of pages to include in the paginator template
-    page_numbers = [n for n in range(start_page, end_page) if 0 < n <= paginator.num_pages]
+    page_numbers = [n for n in range(start_page, end_page) if 0 < n <= num_pages]
 
     return {
         'page_obj': page_obj,
-        'paginator': paginator,
+        'paginator': context['paginator'],
         'page_numbers': page_numbers,
         'show_first': 1 not in page_numbers,
         'show_last': paginator.num_pages not in page_numbers,
