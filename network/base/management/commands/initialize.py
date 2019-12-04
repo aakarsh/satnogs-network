efd645de6e40b6802b4c3c2a8a88da20e7b331d5
Fabian P. Schmidt <kerel@mailbox.org>
2019-12-04 19:33:28 +0100

    management/initialize: Show fixture generation statistics
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- network/base/management/commands/initialize.py
+++ network/base/management/commands/initialize.py
@@ -12,6 +12,10 @@ class Command(BaseCommand):
     help = 'Create initial fixtures'
 
     def handle(self, *args, **options):
+        station_fixture_count = 40
+        observation_fixture_count = 200
+        demoddata_fixture_count = 40
+
         # Migrate
         self.stdout.write("Creating database...")
         call_command('migrate')
@@ -20,20 +24,23 @@ class Command(BaseCommand):
         call_command('loaddata', 'antennas')
         call_command('fetch_data')
 
+        # Update TLEs
+        call_command('update_all_tle')
+
         # Create random fixtures for remaining models
         self.stdout.write("Creating fixtures...")
         StationFactory.create_batch(
-            40, antennas=(Antenna.objects.all().values_list('id', flat=True))
+            station_fixture_count, antennas=(Antenna.objects.all().values_list('id', flat=True))
         )
-        ObservationFactory.create_batch(200)
-        for _ in range(40):
+        self.stdout.write("Added {} stations.".format(station_fixture_count))
+        ObservationFactory.create_batch(observation_fixture_count)
+        self.stdout.write("Added {} observations.".format(observation_fixture_count))
+        for _ in range(demoddata_fixture_count):
             DemodDataFactory.create(
                 payload_demod__data=generate_payload(),
                 payload_demod__filename=generate_payload_name()
             )
-
-        # Update TLEs
-        call_command('update_all_tle')
+        self.stdout.write("Added {} DemodData objects.".format(demoddata_fixture_count))
 
         # Create superuser
         self.stdout.write("Creating a superuser...")
