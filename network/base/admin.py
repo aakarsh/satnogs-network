68e87e6d786ebe202211471f77b2fba32d7aecb2
Alfredos-Panagiotis Damkalis <fredy@fredy.gr>
2019-12-21 22:12:40 +0200

    Run sync_to_db task from transmitter admin page
    Signed-off-by: Alfredos-Panagiotis Damkalis <fredy@fredy.gr>

--- network/base/admin.py
+++ network/base/admin.py
@@ -1,8 +1,12 @@
 """Define functions and settings for the django admin base interface"""
-from django.contrib import admin
+from django.conf.urls import url
+from django.contrib import admin, messages
+from django.shortcuts import redirect
+from django.urls import reverse
 
 from network.base.models import Antenna, DemodData, Observation, Satellite, \
     Station, Tle, Transmitter
+from network.base.tasks import sync_to_db
 from network.base.utils import export_as_csv, export_station_status
 
 
@@ -92,6 +96,30 @@ class TransmitterAdmin(admin.ModelAdmin):
     search_fields = ('uuid', )
     list_filter = ('sync_to_db', )
     readonly_fields = ('uuid', )
+
+    def get_urls(self):
+        """Returns django urls for the Transmitter view
+
+        sync_to_db -- url for the sync_to_db function
+
+        :returns: Django urls for the Transmitter admin view
+        """
+        urls = super(TransmitterAdmin, self).get_urls()
+        my_urls = [
+            url(r'^sync_to_db/$', self.sync_to_db, name='sync_to_db'),
+        ]
+        return my_urls + urls
+
+    def sync_to_db(self, request):  # pylint: disable=R0201
+        """Returns the admin home page, while triggering a Celery sync to DB task
+
+        Forces sync of unsynced demoddata for all transmitters that have set to be synced
+
+        :returns: admin home page redirect with popup message
+        """
+        sync_to_db.delay()
+        messages.success(request, 'Sync to DB task was triggered successfully!')
+        return redirect(reverse('admin:index'))
 
 
 class DemodDataInline(admin.TabularInline):
