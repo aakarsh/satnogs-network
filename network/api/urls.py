b7ef99ba3931117b0890b8689e8af8a13629fea5
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2020-09-29 13:13:05 +0300

    api: Remove 'data' URL registering
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- network/api/urls.py
+++ network/api/urls.py
@@ -6,7 +6,6 @@ from network.api import views
 ROUTER = routers.DefaultRouter()
 
 ROUTER.register(r'jobs', views.JobView, basename='jobs')
-ROUTER.register(r'data', views.ObservationView, basename='data')
 ROUTER.register(r'observations', views.ObservationView, basename='observations')
 ROUTER.register(r'stations', views.StationView, basename='stations')
 ROUTER.register(r'transmitters', views.TransmitterView, basename='transmitters')
