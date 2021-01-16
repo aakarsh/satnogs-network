f5d66e662657d07be14afc3dd0827ac114299464
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2021-01-16 23:30:38 +0200

    Fix Django deprecation warnings
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- network/urls.py
+++ network/urls.py
@@ -2,8 +2,9 @@
 from allauth import urls as allauth_urls
 from avatar import urls as avatar_urls
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import path, re_path
 from django.views.static import serve
 
 from network.api.urls import API_URLPATTERNS
@@ -12,23 +13,23 @@ from network.users.urls import USERS_URLPATTERNS
 
 urlpatterns = [
     # Base urls
-    url(r'^', include(BASE_URLPATTERNS)),
-    url(r'^admin/', admin.site.urls),
-    url(r'^users/', include(USERS_URLPATTERNS)),
-    url(r'^accounts/', include(allauth_urls)),
-    url(r'^avatar/', include(avatar_urls)),
-    url(r'^api/', include(API_URLPATTERNS))
+    path('', include(BASE_URLPATTERNS)),
+    path('admin/', admin.site.urls),
+    path('users/', include(USERS_URLPATTERNS)),
+    path('accounts/', include(allauth_urls)),
+    path('avatar/', include(avatar_urls)),
+    path('api/', include(API_URLPATTERNS))
 ]
 
 # Auth0
 if settings.AUTH0:
-    urlpatterns += [url(r'^', include('auth0login.urls'))]
+    urlpatterns += [path('', include('auth0login.urls'))]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
 
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
--- network/users/urls.py
+++ network/users/urls.py
@@ -1,12 +1,12 @@
 """Django users URL routings for SatNOGS Network"""
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from network.users import views
 
 USERS_URLPATTERNS = (
     [
-        url(r'^redirect/$', views.UserRedirectView.as_view(), name='redirect_user'),
-        url(r'^update/$', views.UserUpdateView.as_view(), name='update_user'),
-        url(r'^(?P<username>[\w.@+-]+)/$', views.view_user, name='view_user'),
+        path('redirect/', views.UserRedirectView.as_view(), name='redirect_user'),
+        path('update/', views.UserUpdateView.as_view(), name='update_user'),
+        re_path(r'^(?P<username>[\w.@+-]+)/$', views.view_user, name='view_user'),
     ], 'users'
 )
